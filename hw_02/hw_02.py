f34f405997f385f278407e0ed6eba1a1ecd8201e
itsanimepotato <150068258+itsanimepotato@users.noreply.github.com>
2026-02-03 05:59:57 -0500

    amogus robot

--- hw_02/hw_02.py
+++ hw_02/hw_02.py
@@ -1,14 +1,17 @@
 
 def setup():
-    size(500,500)
-    
+    size(250,250) #window size
     
 def draw():
-    head(width/2,height/5)
+    background(220)
     
+    fill(255,0,0) #red
+    rect(100,50,50,100) #body
     
-def head(x,y):
-    head_length = 100
-    square(x,y,head_length)
+    rect(75,75,25,50) #backpack
     
+    rect(130,150,15,25) #leg r
+    rect(110,150,15,25) #leg l
     
+    fill(0,255,255) #cyan
+    circle(150,75,25) #glass
