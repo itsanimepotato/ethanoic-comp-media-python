75923be4a0777912f064f4e5b7e339acbe6aac7b
Ethan Cheung <ethancheung.12332100@gmail.com>
2026-02-04 23:16:10 -0500

    yinyang core

--- hw/hw_04.py
+++ hw/hw_04.py
@@ -8,4 +8,15 @@ def draw():
     circle(width/2,height/2,250)
     
     fill(0)
-    circle(width/2,height/4,125)
+    arc(width/2,height/2,250,250,HALF_PI,HALF_PI*3,CHORD)
+    
+    circle(width/2,height/2-height/8,125)
+    
+    fill(255)
+    no_stroke()
+    circle(width/2,height/2+height/8,125)
+    
+    circle(width/2,height/2-height/8,20)
+    fill(0)
+    circle(width/2,height/2+height/8,20)
+    
