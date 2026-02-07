9c630dc5820c276a1a51694eedad38796ef3c4b0
Ethan <echeung60@stuy.edu>
2026-02-06 21:05:35 -0500

    a

--- hw/hw_05.py
+++ hw/hw_05.py
@@ -1,31 +1,36 @@
 
-press = False
 
 def setup():
     size(500,500)
+    global press
+    press = False
+
     
 def draw():
+    background(0)
     fill(255)
     rect(150,50,200,400)
     
-    fill(255,0,0,50)
-    circle(250,150,100)
-    fill(255,255,0,50)
-    circle(250,250,100)
-    fill(0,255,0,50)
-    circle(250,350,100)
-    
-    circ_color(150,press,color(255,0,0))
-    circ_color(250,press,color(255,255,0))
-    circ_color(350,press,color(0,0,255))
+    if press == True:
+        circ_color(150,press,color(255,0,0))
+        circ_color(250,press,color(255,255,0))
+        circ_color(350,press,color(0,255,0))
+        print(press)
+    else:
+        fill(255,0,0,50)
+        circle(250,150,100)
+        fill(255,255,0,50)
+        circle(250,250,100)
+        fill(0,255,0,50)
+        circle(250,350,100)
+        print(press)
 
     
 def circ_color(y,press,c):
-    if press:
-        fill(c)
-        circle(250,y,100)
+    fill(c)
+    circle(250,y,100)
 
 def mouse_pressed():
-    circ_color(150,press,color(255,0,0))
-    circ_color(250,press,color(255,0,0))
-    circ_color(350,press,color(255,0,0))
+    press = True
+    print(press)
+    
