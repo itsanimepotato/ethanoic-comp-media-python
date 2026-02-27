bb53d0d5872b65ca816ad6878f27de10d4d1fca4
Ethan <echeung60@stuy.edu>
2026-02-27 09:42:03 -0500

    relocating to folders

---
+++ cw/cw_10/cw_10_random.py
@@ -0,0 +1,28 @@
+
+star_list = []
+num_stars = 500
+
+def setup():
+    size(500,500)
+    background(0)
+    
+    for i in range(num_stars):
+        star_list.append({
+            "x":random(width),
+            "y":random(height),
+            "c":color(random(255))})
+    
+    
+def draw():
+    real_stars()
+    
+def real_stars():
+    background(0)
+    for i in range(len(star_list)):
+        rand_weight = 2
+        if frame_count % 2 == 0:
+            rand_weight = random(5)
+        
+        stroke_weight(rand_weight)
+        stroke(star_list[i]["c"])
+        point(star_list[i]["x"],star_list[i]["y"])
---
+++ cw/cw_11/cw_11_image.py
@@ -0,0 +1,11 @@
+
+
+
+def setup():
+    size(500,500)
+    global img
+    img = load_image("../img/RedPurpleYoyleBerry.png")
+
+    
+def draw():
+    image(img,0,0)
